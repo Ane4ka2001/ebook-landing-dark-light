058467bc8974d242df024200d55b513f3f558ec4
Ane4ka <masharueva@yandex.ru>
2024-03-28 09:05:17 +0300

    добавление отслеживания клика по корзине

--- src/home/script.js
+++ src/home/script.js
@@ -1,1 +1,14 @@
+function installCartFeature() {
+    const cartButton = document.querySelector("#cart-button");
+    const cartButtonClickListener = () => {
+        console.log("Кликнул")
+    };
+    console.log(cartButton);
+    cartButton.addEventListener("click", cartButtonClickListener);
+}
 
+function main() {
+    installCartFeature();
+}
+
+document.addEventListener("DOMContentLoaded", main)
