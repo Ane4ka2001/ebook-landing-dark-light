1273c0fc11af88717d5d1ef01525f4c1e7ae952a
Ryan Wright <SivanoFFDS@gmail.com>
2024-04-04 12:30:42 +0700

    feat: init dialog element dynamically

--- src/home/script.js
+++ src/home/script.js
@@ -1,14 +1,29 @@
 function installCartFeature() {
-    const cartButton = document.querySelector("#cart-button");
-    const cartButtonClickListener = () => {
-        console.log("Кликнул")
-    };
-    console.log(cartButton);
-    cartButton.addEventListener("click", cartButtonClickListener);
+  const cartButton = document.querySelector("#cart-button");
+
+  const dialog = document.createElement("dialog");
+
+  dialog.innerHTML = "<div>Что-то</div>";
+  const dialogContent = dialog.children[0];
+  document.body.appendChild(dialog);
+
+  dialog.addEventListener("click", () => {
+    dialog.close();
+  });
+  dialogContent.addEventListener("click", (event) => {
+    event.stopPropagation();
+  });
+
+  const cartButtonClickListener = () => {
+    dialog.showModal();
+  };
+  dialog.showModal();
+
+  cartButton.addEventListener("click", cartButtonClickListener);
 }
 
 function main() {
-    installCartFeature();
+  installCartFeature();
 }
 
-document.addEventListener("DOMContentLoaded", main)
+document.addEventListener("DOMContentLoaded", main);
