45ab1e2f8d8e53111df960b9caad1347af25dca6
Ryan Wright <SivanoFFDS@gmail.com>
2024-04-15 13:03:53 +0700

    feat: dynamic cart

--- src/home/script.js
+++ src/home/script.js
@@ -1,9 +1,54 @@
+const CART_STORE = {
+  items: [
+    {
+      name: "Atomic One’s",
+      price: 30,
+      count: 1,
+      imageSrc: "./Book-2.png",
+    },
+    {
+      name: "Atomic One’s 2",
+      price: 30,
+      count: 1,
+      imageSrc: "./Book-2.png",
+    },
+    {
+      name: "Atomic One’s 3",
+      price: 30,
+      count: 1,
+      imageSrc: "./Book-2.png",
+    },
+  ],
+  getBookHtml: (book) => {
+    const result = `<div>${book.name} $${book.price}</div>`;
+
+    return result;
+  },
+  remove: (itemIndex) => {
+    console.log("Удалить книжку", itemIndex);
+  },
+  getTotal() {
+    let sum = 0;
+
+    for (let i = 0; i < this.items.length; i++) {
+      sum = sum + this.items[i].price;
+    }
+    return sum;
+  },
+  getCardsHTML() {
+    const bookCards = this.items.map(this.getBookHtml).join("");
+    return bookCards;
+  },
+};
+
 function installCartFeature() {
   const cartButton = document.querySelector("#cart-button");
 
   const dialog = document.createElement("dialog");
   dialog.classList.add("dialog");
 
+  const bookCards = CART_STORE.getCardsHTML();
+  const cartTotal = CART_STORE.getTotal();
   dialog.innerHTML = `
   <div class='dialog-content'>
     <div class='dialog-header'>
@@ -14,6 +59,14 @@ function installCartFeature() {
         </svg>
       </button>
     </div>
+    <div class="dialog-main">${bookCards}</div>
+    <div class="dialog-footer">
+      <div>
+        <span>Sub-Total</span>
+        <strong>$ ${cartTotal} USD</strong>
+      </div>
+      <button class="dialog-checkout" id="checkout">Continue to Checkout</button>
+    </div>
   </div>
   `;
   const closeButton = dialog.querySelector("#close");
@@ -23,6 +76,9 @@ function installCartFeature() {
   const closeDialog = () => {
     dialog.close();
   };
+
+  const checkoutButton = dialog.querySelector("#checkout");
+  checkoutButton.addEventListener("click", closeDialog);
 
   dialog.addEventListener("click", closeDialog);
   closeButton.addEventListener("click", closeDialog);
